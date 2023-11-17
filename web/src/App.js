9126b742a304db148c2199fb32e9afb68d769ac3
JUKKarol <karolprzybylak2@gmail.com>
2023-11-17 21:38:51 +0100

    Init react

---
+++ web/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyTasks
+        </a>
+        <nav>
+          <a href="" className="logo">
+            Login
+          </a>
+          <a href="" className="logo">
+            Register
+          </a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
