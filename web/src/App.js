1faabdbff05265bad85f1d7b3f68c4b751db0985
JUKKarol <karolprzybylak2@gmail.com>
2023-11-19 13:46:13 +0100

    Add react do

--- web/src/App.js
+++ web/src/App.js
@@ -1,58 +1,32 @@
 import "./App.css";
+import Task from "./Task";
+import Header from "./Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          MyTasks
-        </a>
-        <nav>
-          <a href="" className="logo">
-            Login
-          </a>
-          <a href="" className="logo">
-            Register
-          </a>
-        </nav>
-      </header>
-      <div className="task">
-        <div className="image">
-          <img
-            src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
-            alt="xd"
-          />
-        </div>
-        <div className="texts">
-          <h2>Repair my BMW</h2>
-          <p>SLIDER</p>
-        </div>
-      </div>
-      <div className="task">
-        <div className="image">
-          <img
-            src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
-            alt="xd"
-          />
-        </div>
-        <div className="texts">
-          <h2>Repair my BMW</h2>
-          <p>SLIDER</p>
-        </div>
-      </div>
-      <div className="task">
-        <div className="image">
-          <img
-            src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
-            alt="xd"
-          />
-        </div>
-        <div className="texts">
-          <h2>Repair my BMW</h2>
-          <p>SLIDER</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route
+        index
+        element={
+          <main>
+            <Header />
+            <Task />
+            <Task />
+            <Task />
+          </main>
+        }
+      />
+      <Route
+        path={"/login"}
+        element={
+          <main>
+            <Header />
+            <div>login page</div>
+          </main>
+        }
+      />
+    </Routes>
   );
 }
 
---
+++ web/src/Task.js
@@ -0,0 +1,16 @@
+export default function Task() {
+  return (
+    <div className="task">
+      <div className="image">
+        <img
+          src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
+          alt="xd"
+        />
+      </div>
+      <div className="texts">
+        <h2>Repair my BMW</h2>
+        <p>SLIDER</p>
+      </div>
+    </div>
+  );
+}
