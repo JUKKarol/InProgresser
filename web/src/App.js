a5bf8b4860ded6042845e4cc69dec8a8a822b61e
JUKKarol <karolprzybylak2@gmail.com>
2023-11-19 11:41:29 +0100

    Add homepage

--- web/src/App.js
+++ web/src/App.js
@@ -16,6 +16,42 @@ function App() {
           </a>
         </nav>
       </header>
+      <div className="task">
+        <div className="image">
+          <img
+            src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
+            alt="xd"
+          />
+        </div>
+        <div className="texts">
+          <h2>Repair my BMW</h2>
+          <p>SLIDER</p>
+        </div>
+      </div>
+      <div className="task">
+        <div className="image">
+          <img
+            src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
+            alt="xd"
+          />
+        </div>
+        <div className="texts">
+          <h2>Repair my BMW</h2>
+          <p>SLIDER</p>
+        </div>
+      </div>
+      <div className="task">
+        <div className="image">
+          <img
+            src="https://bmwsklep.pl/wp-content/uploads/2022/09/boczne-logo-m-bmw-serii-2-3-4-x5-x6-x7-modele-g-m-performance-czarne-51145a4b373-2.jpg"
+            alt="xd"
+          />
+        </div>
+        <div className="texts">
+          <h2>Repair my BMW</h2>
+          <p>SLIDER</p>
+        </div>
+      </div>
     </main>
   );
 }
