d63ec0b013260f630dacfe2b3d5db7484df03135
JUKKarol <karolprzybylak2@gmail.com>
2023-11-19 17:10:20 +0100

    Add missing headers in forms

--- web/src/pages/RegisterPage.js
+++ web/src/pages/RegisterPage.js
@@ -1,6 +1,7 @@
 export default function RegisterPage() {
   return (
     <form className="register">
+      <h1>Register</h1>
       <input type="text" placeholder="Username"></input>
       <input type="password" placeholder="Password"></input>
       <button>Register</button>
